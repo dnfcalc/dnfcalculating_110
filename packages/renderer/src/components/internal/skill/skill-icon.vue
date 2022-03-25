96898753950ccff23eea5cc624bd0d601bea0e08
西瓜 <8441078+i_melon@user.noreply.gitee.com>
2022-03-25 14:44:19 +0000

    !29 技能树完善 * 调整技能树组件

--- packages/renderer/src/components/internal/skill/skill-icon.vue
+++ packages/renderer/src/components/internal/skill/skill-icon.vue
@@ -4,12 +4,35 @@
 		name: "skill-icon",
 		components: {  },
 		props: {
-			
+			skill: {
+				type: Object,
+				required: true
+			}
 		},
 		setup(props, { emit }) {
+			function increaseSkillLv(e: MouseEvent) {
+				if(props.skill.learn < props.skill.master) {
+					props.skill.learn++;
+				}
+			}
+			function reduceSkillLv(e: MouseEvent) {
+				if(props.skill.learn > props.skill.default) {
+					props.skill.learn--;
+				}
+			}
+
 			return () => {
 				return (
-					<div class="skill-icon"></div>
+					<div class="skill-icon" onClick={increaseSkillLv}>
+						<img src={props.skill.icon} />
+						<div class="skill-lv">
+							{ props.skill.learn == props.skill.master ? (
+								<span class="M">M</span>) : (
+								<span class="lv">Lv</span>)
+							}
+							<span class="num">{props.skill.learn}</span>
+						</div>
+					</div>
 				)
 			}
 		}
@@ -18,16 +41,60 @@
 <style scoped lang="scss">
 .skill-icon {
 	display: inline-block;
-	width: 60px;
-	height: 80px;
-	border: 1px solid transparent;
-	border-radius: 5px;
-	background-color: #000;
-	cursor: pointer;
+	width: 36px; // 50
+	height: 56px; // 70
+	border: 1px solid #000;
+	border-radius: 2px;
+	background-color: #2F2F2F;
 	
 	&:hover {
 		border-color: #64ADD0;
 	}
+	
+	> img {
+		//margin: 3px 3px 1px 3px;
+		margin: 2px 2px 2px 2px;
+		display: block;
+		width: 30px;
+		//width: 42px;
+		//height: 42px;
+		height: 30px;
+		border: 1px solid #000;
+	}
+	
+	.skill-lv {
+		margin: 0 5px;
+		background-color: #000;
+		text-align: center;
+		font-size: 12px;
+		height: 18px;
+		line-height: 18px;
+		width: calc(100% - 4px);
+		margin-left: 2px;
+		
+		.lv {
+			margin-right: 3px;
+			color: #FFF;
+		}
+		
+		.M {
+			margin-right: 5px;
+			border: 1px solid #C6A228;
+			border-radius: 3px;
+			color: #C6A228;
+			display: inline-block;
+			height: 11px;
+			line-height: 11px;
+			padding: 0 1px 0 2px;
+			
+			+ .num {
+				color: #C6A228;
+			}
+		}
+		
+		.num {
+			color: #FFF;
+		}
+	}
 }
-
 </style>
--- packages/renderer/src/components/internal/skill/skill-panel.vue
+++ packages/renderer/src/components/internal/skill/skill-panel.vue
@@ -1,193 +1,158 @@
+
 <script lang="tsx">
-	import { defineComponent, onMounted, reactive, ref, computed } from "vue"
+	import { defineComponent, onMounted, reactive, renderList } from "vue"
 	import SkillIcon from "./skill-icon.vue"
+  	import { ICharacterInfo } from "@/api/character/type"
+  	import { useCharacterStore } from "@/store"
+
+
+	import { skills as baseSkills, ISkillInfoForSkillPanel, ISkillTreeForSkillPanel } from './skill-set'
+
+	interface IConfigForSkillPanel {
+		ey: number,
+		ex: number,
+		sy: number,
+		sx: number,
+		s: ISkillInfoForSkillPanel,
+		b: ISkillInfoForSkillPanel
+	}
+
 	export default defineComponent({
 		name: "skill-panel",
 		components: { SkillIcon },
 		props: {
+			character: {
+				type: String,
+				default: "重霄·弹药专家·女"
+			}
 		},
 		setup(props, { emit }) {
 			
-			const skills = reactive([
-				{
-					lv: 1,
-					sks: [
-						{ id: 1, name: '冰结师皮甲专精', col: 1, sp: 0, jt: 1, max: 1, default: 1 },
-						{ id: 2, name: '不死之身', col: 2, sp: 0, jt: 1, max: 1, default: 1 },
-						{ id: 3, name: '黑暗之眼', col: 3, sp: 0, jt: 1, max: 1, default: 1 },
-						{ id: 4, name: '基础防具精通', col: 4, sp: 0, jt: 1, max: 1, default: 1 },
-						{ id: 7001, name: '魔法水球', col: 5, sp: 0, jt: 1, max: 1, default: 1 },
-						{ id: 7002, name: '魔法旋风', col: 7, sp: 0, jt: 1, max: 1, default: 1 },
-					]
-				},
-				{
-					lv: 5,
-					sks: [
-						{ id: 7003, name: '擒拿掌', col: 4, sp: 0, jt: 1, max: 1, default: 1 },
-						{ id: 7004, name: '魔球连射', col: 7, sp: 0, jt: 1, max: 1, default: 1 },
-					]
-				},
-				{
-					lv: 10,
-					sks: [
-						{ id: 7005, name: '幽冥火', col: 3, sp: 0, jt: 1, max: 1, default: 1 },
-						{ id: 7006, name: '瞬移', col: 6, sp: 0, jt: 1, max: 1, default: 1 },
-						{ id: 7007, name: '旋火盾', col: 8, sp: 0, jt: 1, max: 1, default: 1 },
-					]
-				},
-				{
-					lv: 15,
-					sks: [
-						{ id: 7008, name: '', col: 4, sp: 0, jt: 1, max: 1, default: 1 },
-						{ id: 7009, name: '', col: 7, sp: 0, jt: 1, max: 1, default: 1 },
-						{ id: 7010, name: '', col: 9, sp: 0, jt: 1, max: 1, default: 1 },
-					]
-				},
-				{
-					lv: 20,
-					sks: [
-						{ id: 7011, name: '', col: 5, sp: 0, jt: 1, max: 1, default: 1 },
-					]
-				},
-				{
-					lv: 25,
-					sks: [
-						{ id: 7012, name: '', col: 4, sp: 0, jt: 1, max: 1, default: 1  },
-						{ id: 7013, name: '', col: 6, sp: 0, jt: 1, max: 1, default: 1 },
-						{ id: 7014, name: '', col: 8, sp: 0, jt: 1, max: 1, default: 1 },
-					]
-				},
-				{
-					lv: 30,
-					sks: [
-						{ id: 7015, name: '', col: 3, sp: 0, jt: 1, max: 1, default: 1, before: [7012] },
-						{ id: 7016, name: '', col: 5, sp: 0, jt: 1, max: 1, default: 1, before: [7012] },
-						{ id: 7017, name: '', col: 8, sp: 0, jt: 1, max: 1, default: 1, before: [7014] },
-						{ id: 7018, name: '', col: 9, sp: 0, jt: 1, max: 1, default: 1, before: [7010] },
-					]
-				},
-				{
-					lv: 35,
-					sks: [
-						{ id: 7019, name: '', col: 3, sp: 0, jt: 1, max: 1, default: 1, before: [7015] },
-						{ id: 7020, name: '', col: 5, sp: 0, jt: 1, max: 1, default: 1, before: [7016] },
-					]
-				},
-				{
-					lv: 40,
-					sks: [
-						{ id: 7021, name: '', col: 5, sp: 0, jt: 1, max: 1, default: 1 },
-					]
-				},
-				{
-					lv: 45,
-					sks: [
-						{ id: 7022, name: '', col: 4, sp: 0, jt: 1, max: 1, default: 1, before: [7021] },
-						{ id: 7023, name: '', col: 6, sp: 0, jt: 1, max: 1, default: 1, before: [7021] },
-					]
-				},
-				{
-					lv: 50,
-					sks: [
-						{ id: 7024, name: '', col: 6, sp: 0, jt: 1, max: 1, default: 1 },
-						{ id: 7025, name: '', col: 7, sp: 0, jt: 1, max: 1, default: 1 },
-					]
-				},
-				{
-					lv: 60,
-					sks: [
-						{ id: 7026, name: '', col: 6, sp: 0, jt: 1, max: 1, default: 1 },
-					]
-				},
-				{
-					lv: 70,
-					sks: [
-						{ id: 7027, name: '', col: 5, sp: 0, jt: 1, max: 1, default: 1 },
-						{ id: 7028, name: '', col: 7, sp: 0, jt: 1, max: 1, default: 1 },
-						{ id: 7029, name: '', col: 9, sp: 0, jt: 1, max: 1, default: 1 },
-					]
-				},
-				{
-					lv: 80,
-					sks: [
-						{ id: 7030, name: '', col: 7, sp: 0, jt: 1, max: 1, default: 1 },
-						{ id: 7031, name: '', col: 9, sp: 0, jt: 1, max: 1, default: 1 },
-					]
-				},
-				{
-					lv: 90,
-					sks: [
-						{ id: 7032, name: '', col: 6, sp: 0, jt: 1, max: 1, default: 1 },
-						{ id: 7033, name: '', col: 8, sp: 0, jt: 1, max: 1, default: 1 },
-					]
-				},
-				{
-					lv: 100,
-					sks: [
-						{ id: 7034, name: '', col: 5, sp: 0, jt: 1, max: 1, default: 1 },
-						{ id: 7035, name: '', col: 7, sp: 0, jt: 1, max: 1, default: 1 },
-						{ id: 7036, name: '', col: 8, sp: 0, jt: 1, max: 1, default: 1 }
-					]
-				},
-			])
+			const skills = reactive<ISkillTreeForSkillPanel[]>([])
+
+		 	async function initSkills() {
+				const characterInfoState = useCharacterStore()
+      			await characterInfoState.get_character_info(props.character)
+      			let basicInfo = characterInfoState[props.character]?.basicInfo as ICharacterInfo
+
+				let sks = baseSkills.get(props.character);
+				if(sks && sks.length > 0) {
+					for(let item of sks) {
+						let tk = basicInfo.skillInfo.find(x => x.name == item.name);
+						if(tk) {
+							item.master = tk.level_master;
+							item.max = tk.level_max;
+							item.skillInfo = tk;
+							item.icon = `./images/characters/${props.character}/skill/${item.name}.png`
+						}
+						else {
+							// 这里需要修改成一个默认图标
+							item.icon = `https://gitee.com/FeelingsCoder/picture-resource-station/raw/master/equipment/arms/swordman/sswd/192.png`
+						}
+
+						let ios = skills.find(x => x.lv == item.row);
+						if(!ios) {
+							ios = { lv: item.row, sks: [] };
+							skills.push(ios);
+						}
+						ios.sks.push(item);
+					}
+				}
+			}
 			
-			const drawData = computed(() => {
-				let ss = [];
-				let data = [];
+			function initDrawData() {
+				let ss = [] as ISkillInfoForSkillPanel[];
+				let data = [] as IConfigForSkillPanel[];
 				for(var i = 0; i < skills.length; i++) {
 					var group = skills[i]
 					for(var s of group.sks) {
 						if(s.before) {
 							for(var bb of s.before) {
-								var b = ss.find(x => x.id == bb);
+								var b = ss.find(x => x.id == bb.id);
 								if(b) {
 									data.push({
 										ey: i,
 										ex: s.col,
-										sy: b.y,
+										sy: b.y ?? 0,
 										sx: b.col,
-										s,
-										b
+										s: s,
+										b: b
 									});
 								}
 							}
 						}
 						s.y = i;
+						s.learn = s.default;
 						ss.push(s);
 					}
 				}
 				return data;
-			});
+			};
 			
-			function draw() {
-				let canvas = document.getElementById('canvas-for-skill-back');
+			let config = {
+				cols: 11, // 总列数
+				lineW: 6, // 连线宽度
+				marginLeft: 30, // 左侧等级标识列宽度
+				row: {
+					h: 56 + 10 + 10, // 单元格高度 + 单元格上下间距
+					padding: 10, // 单元格上下间距
+
+					w: 36 + 20, // 单元格宽度 : icon 宽度 + 间距
+					margin: 20 // 单元格左右间距
+				},
+				icon: {
+					ih: 56, // icon 高度
+					iw: 36, // icon 宽度
+				}
+			}
+
+			async function draw() {
+				await initSkills();
+
+				let canvas = document.getElementById('canvas-for-skill-back') as HTMLCanvasElement;
 				let ctx = canvas.getContext("2d");
-				canvas.width = 848;
-				canvas.height = skills.length * 100;
-				
-				for(var i = 0; i < skills.length; i++) {
-					ctx.fillStyle = i % 2 == 0 ? 'rgba(0, 0, 0, 0.3)' : 'rgba(30, 30, 30, 0.3)';
-					ctx.fillRect(0, i * 100, canvas.width, 100);
+				canvas.width = config.cols * config.row.w + config.marginLeft;
+				canvas.height = skills.length * config.row.h;
+				if(ctx) {
+					for(var i = 0; i < skills.length; i++) {
+						ctx.fillStyle = i % 2 == 0 ? 'rgba(0, 0, 0, 0.3)' : 'rgba(30, 30, 30, 0.3)';
+						ctx.fillRect(0, i * config.row.h, canvas.width, config.row.h);
+					}
+					ctx.fillStyle="#593D22";
+					let dd = initDrawData();
+					for(var item of dd) {
+						let x = (item.sx - 1) * config.row.w + config.icon.iw / 2 + config.marginLeft - config.lineW / 2 + 1;
+						let y = item.sy * config.row.h + (config.row.h - config.row.padding);
+						let h = (item.ey - item.sy) * config.row.h - config.icon.ih - config.lineW;
+						let w = (item.ex - item.sx) * config.row.w;
+						
+						if(h < 0) {
+							x = x + config.icon.iw / 2 + config.lineW / 2;
+							y = y + h / 2;
+							w = w - config.icon.iw - config.lineW
+							ctx.fillRect(x, y, w, config.lineW);
+
+							ctx.beginPath();
+							ctx.moveTo(x + w, y - config.lineW + 1);
+							ctx.lineTo(x + w, y + config.lineW * 2 - 1);
+							ctx.lineTo(x + w + config.lineW, y + config.lineW / 2);
+							ctx.closePath();
+							ctx.fill();
+						}
+						else {
+							ctx.fillRect(x, y, config.lineW, h / 2);
+							ctx.fillRect(x, y + h / 2, w, config.lineW);
+							ctx.fillRect(x + w, y + h / 2, config.lineW, h / 2);
+							
+							ctx.beginPath();
+							ctx.moveTo(x + w - config.lineW + 1, y + h);
+							ctx.lineTo(x + w + config.lineW * 2 - 1, y + h);
+							ctx.lineTo(x + w + config.lineW / 2, y + h + config.lineW);
+							ctx.closePath();
+							ctx.fill();
+						}
+					}
 				}
-				ctx.fillStyle="#593D22";
-				for(var item of drawData.value) {
-					let x = (item.sx - 1) * 75 + 30 + 27;
-					let y = item.sy * 100 + 90;
-					let h = (item.ey - item.sy) * 100 - 86;
-					let w = (item.ex - item.sx) * 75;
-					
-					ctx.fillRect(x, y, 6, h / 2);
-					ctx.fillRect(x, y + h / 2, w, 6);
-					ctx.fillRect(x + w, y + h / 2, 6, h / 2);
-					
-					ctx.beginPath();
-					ctx.moveTo(x + w - 5, y + h);
-					ctx.lineTo(x + w + 11, y + h);
-					ctx.lineTo(x + w + 3, y + h + 6);
-					ctx.closePath();
-					ctx.fill();
-				}
-				
 			}
 			
 			onMounted(draw)
@@ -195,15 +160,15 @@
 			return () => {
 				return (
 					<div class="skill-panel">
-						<el-scrollbar>
-							<canvas id="canvas-for-skill-back"></canvas>
-							{ skills.map(skm =>
-								<div class="skills-group">
-									<div class="lv-tips">{skm.lv % 10 == 0 || skm.lv == 1 ? skm.lv : ''}</div>
-									{ skm.sks.map((s, i) => <skill-icon class="skill-item" style={'left: ' + ((s.col - 1) * 75 + 30) + 'px'}></skill-icon>) }
-								</div>)
-							}
-						</el-scrollbar>
+						<canvas id="canvas-for-skill-back"></canvas>
+						{ renderList(skills, skm =>
+							<div class="skills-group" style={ `height: ${config.row.h}px` }>
+								<div class="lv-tips">{skm.lv % 10 == 0 || skm.lv == 1 ? skm.lv : ''}</div>
+								{ renderList(skm.sks, (s, i) => 
+									<skill-icon skill={s} class="skill-item" style={'left: ' + ((s.col - 1) * config.row.w + config.marginLeft) + 'px'}></skill-icon>)
+								}
+							</div>)
+						}
 					</div>
 				)
 			}
@@ -212,12 +177,10 @@
 </script>
 <style scoped lang="scss">
 .skill-panel {
-	height: 1600px;
+	height: 100%;
 	position: relative;
-	
-	
+	font-size: 12px;
 	.skills-group {
-		height: 100px;
 		position: relative;
 		
 		.lv-tips {
