548dd6a6f17fc1231c479d295377abab21827922
西瓜 <8441078+i_melon@user.noreply.gitee.com>
2022-03-29 14:55:21 +0000

    !31 装备信息 * 添加武器图片

--- packages/renderer/src/components/internal/skill/skill-icon.vue
+++ packages/renderer/src/components/internal/skill/skill-icon.vue
@@ -1,100 +1,101 @@
 <script lang="tsx">
-	import { defineComponent, reactive } from "vue"
-	export default defineComponent({
-		name: "skill-icon",
-		components: {  },
-		props: {
-			skill: {
-				type: Object,
-				required: true
-			}
-		},
-		setup(props, { emit }) {
-			function increaseSkillLv(e: MouseEvent) {
-				if(props.skill.learn < props.skill.master) {
-					props.skill.learn++;
-				}
-			}
-			function reduceSkillLv(e: MouseEvent) {
-				if(props.skill.learn > props.skill.default) {
-					props.skill.learn--;
-				}
-			}
+  import { defineComponent, reactive } from "vue"
+  export default defineComponent({
+    name: "skill-icon",
+    components: {},
+    props: {
+      skill: {
+        type: Object,
+        required: true
+      }
+    },
+    setup(props, { emit }) {
+      function increaseSkillLv(e: MouseEvent) {
+        if (props.skill.learn < props.skill.master) {
+          props.skill.learn++
+        }
+      }
+      function reduceSkillLv(e: MouseEvent) {
+        if (props.skill.learn > props.skill.default) {
+          props.skill.learn--
+        }
+      }
 
-			return () => {
-				return (
-					<div class="skill-icon" onClick={increaseSkillLv}>
-						<img src={props.skill.icon} />
-						<div class="skill-lv">
-							{ props.skill.learn == props.skill.master ? (
-								<span class="M">M</span>) : (
-								<span class="lv">Lv</span>)
-							}
-							<span class="num">{props.skill.learn}</span>
-						</div>
-					</div>
-				)
-			}
-		}
-	})
+      return () => {
+        return (
+          <div class="skill-icon" onClick={increaseSkillLv}>
+            <img src={props.skill.icon} />
+            <div class="skill-lv">
+              {props.skill.learn == props.skill.master ? (
+                <span class="M">M</span>
+              ) : (
+                <span class="lv">Lv</span>
+              )}
+              <span class="num">{props.skill.learn}</span>
+            </div>
+          </div>
+        )
+      }
+    }
+  })
 </script>
 <style scoped lang="scss">
-.skill-icon {
-	display: inline-block;
-	width: 36px; // 50
-	height: 56px; // 70
-	border: 1px solid #000;
-	border-radius: 2px;
-	background-color: #2F2F2F;
-	
-	&:hover {
-		border-color: #64ADD0;
-	}
-	
-	> img {
-		//margin: 3px 3px 1px 3px;
-		margin: 2px 2px 2px 2px;
-		display: block;
-		width: 30px;
-		//width: 42px;
-		//height: 42px;
-		height: 30px;
-		border: 1px solid #000;
-	}
-	
-	.skill-lv {
-		margin: 0 5px;
-		background-color: #000;
-		text-align: center;
-		font-size: 12px;
-		height: 18px;
-		line-height: 18px;
-		width: calc(100% - 4px);
-		margin-left: 2px;
-		
-		.lv {
-			margin-right: 3px;
-			color: #FFF;
-		}
-		
-		.M {
-			margin-right: 5px;
-			border: 1px solid #C6A228;
-			border-radius: 3px;
-			color: #C6A228;
-			display: inline-block;
-			height: 11px;
-			line-height: 11px;
-			padding: 0 1px 0 2px;
-			
-			+ .num {
-				color: #C6A228;
-			}
-		}
-		
-		.num {
-			color: #FFF;
-		}
-	}
-}
+  .skill-icon {
+    display: inline-block;
+    width: 36px; // 50
+    height: 56px; // 70
+    border: 1px solid #000;
+    border-radius: 2px;
+    background-color: #2f2f2f;
+
+    &:hover {
+      border-color: #64add0;
+    }
+
+    > img {
+      //margin: 3px 3px 1px 3px;
+      margin: 2px 2px 2px 2px;
+      display: block;
+      width: 30px;
+      //width: 42px;
+      //height: 42px;
+      height: 30px;
+      border: 1px solid #000;
+    }
+
+    .skill-lv {
+      margin: 0 5px;
+      background-color: #000;
+      text-align: center;
+      font-size: 12px;
+      height: 18px;
+      line-height: 18px;
+      width: calc(100% - 4px);
+      margin-left: 2px;
+
+      .lv {
+        margin-right: 3px;
+        color: #fff;
+      }
+
+      .M {
+        margin-right: 2px;
+        border: 1px solid #c6a228;
+        border-radius: 3px;
+        color: #c6a228;
+        display: inline-block;
+        height: 10px;
+        line-height: 11px;
+        padding: 0 1px 0 2px;
+
+        + .num {
+          color: #c6a228;
+        }
+      }
+
+      .num {
+        color: #fff;
+      }
+    }
+  }
 </style>
