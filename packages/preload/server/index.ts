b6beee497f58309902e1fe3f2570b2e28e022fcc
西瓜° <8441078+i_melon@user.noreply.gitee.com>
2022-05-13 01:20:06 +0800

    端口判断

--- packages/preload/server/index.ts
+++ packages/preload/server/index.ts
@@ -9,15 +9,39 @@ let instance: child_process.ChildProcess
  * @returns
  */
 export function startServer() {
-  // TODO 启动python api 待改进 后续添加端口占用判断等
-  if (process.platform == "win32") {
-    instance = child_process.spawn(`python`, [`api/main.py`, `${port}`])
-  } else {
-    instance = child_process.spawn("python3", [`${__dirname}/../../api/main.py`, `${port}`])
-  }
-  if (instance) {
-    console.log("server started.")
-  }
+  judgeServerOpen(17173).then(res => {
+    if (res) {
+      console.log("17173 is in used")
+      return
+    }
+    if (process.platform == "win32") {
+      // TODO 启动python api 待改进 后续添加端口占用判断等
+      instance = child_process.spawn(`python`, [`api/main.py`, `${port}`])
+    } else {
+      instance = child_process.spawn("python3", [`${__dirname}/../../api/main.py`, `${port}`])
+    }
+    if (instance) {
+      console.log("server started.")
+    }
+  })
+}
+
+/**
+ * 判断端口是否被占用
+ * @param port 端口号
+ * @returns 该端口是否被占用
+ */
+function judgeServerOpen(port: number): Promise<boolean> {
+  let order = `netstat -ano|findstr "${port}"`
+  return new Promise((resolve, reject) => {
+    child_process.exec(order, function (error: any, stdout: any, stderr: any) {
+      if (stdout === "") {
+        resolve(false)
+      } else {
+        resolve(true)
+      }
+    })
+  })
 }
 
 /**
