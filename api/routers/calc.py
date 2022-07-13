c6cdccfd1122afdf43ef325eee2f47f62284e7f8
西瓜° <wxh_email@yeah.net>
2022-07-13 18:12:08 +0800

    dev

--- api/routers/calc.py
+++ api/routers/calc.py
@@ -8,8 +8,9 @@ from utils.apiTools import Return, response
 
 from .token import AlterState, authorize, createToken
 
-calcRouter = APIRouter()
+import itertools
 
+calcRouter = APIRouter()
 
 @calcRouter.post(path="/calc")
 async def calc(setInfo=Body(None), setName=Body(None), state: AlterState = Depends(authorize)):
@@ -21,9 +22,24 @@ async def calc(setInfo=Body(None), setName=Body(None), state: AlterState = Depen
         # raise Exception("无效token")
         # 配置信息
     set.save(alter, setName, setInfo)
-    # 先存档配置信息，再进行计算
-    # 职业
-    return response(data=None)
+    # 取每个部位
+    sort = setInfo["equip_list"]
+    # ["称号", "宠物", "武器", "上衣", "下装", "头肩", "腰带", "鞋", "手镯", "项链", "戒指", "辅助装备", "魔法石", "耳环"]
+    info = {
+        "token":state.token,
+        "results":[]
+    }
+    for item in itertools.product(*sort):
+        module_name = "core.characters." + alter
+        character: Character = importlib.import_module(module_name).classChange()
+        temp = character.calc(setName, list(item), False)
+        store.set("/calc/results/"+temp.get("id"), temp)
+        info["results"].append({
+            "id":temp.get("id"),
+            "total":temp.get("total_data"),
+            "plan":list(item)
+        })
+    return response(data=info)
 
 
 @calcRouter.get(path="/calc/result/{id}")
